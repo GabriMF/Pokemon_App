f3885e3ecc98abef928b766783264b76de9d32ca
GabriMF <108740750+GabriMF@users.noreply.github.com>
2023-01-13 15:07:35 +0100

    git pages ready

---
+++ docs/assets/PokemonView-0eadec39.js
@@ -0,0 +1,1 @@
+import{_ as e,o,c,a as n}from"./index-a01d8fc9.js";const t={},s=n("h2",null,"POKEMONS",-1),a=[s];function _(r,l){return o(),c("main",null,a)}const d=e(t,[["render",_]]);export{d as default};
